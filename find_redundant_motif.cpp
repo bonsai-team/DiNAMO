bonsai-team/DiNAMO

commit 9d56cde9997ce2b119f38314c6d1af40403d8d2a

[FIX?] Fixed wrong method to detect overlapping, to be tested

find_redundant_motif.cpp
@@ -1,41 +1,5 @@
 #include "find_redundant_motif.hpp"
 
-sparse_hash_map<char, unordered_set<char>> iupac_to_included_nucs {
-    {'A', {'A'}},
-    {'C', {'C'}},
-    {'G', {'G'}},
-    {'T', {'T'}},
-    {'R', {'R','A','G'}},
-    {'Y', {'Y','C','T'}},
-    {'S', {'S','G','C'}},
-    {'W', {'W','A','T'}},
-    {'K', {'K','G','T'}},
-    {'M', {'M','A','C'}},
-    {'B', {'B','Y','S','K','C','G','T'}},
-    {'D', {'D','R','W','K','A','G','T'}},
-    {'H', {'H','Y','W','M','A','C','T'}},
-    {'V', {'V','R','S','M','A','C','G'}},
-    {'N', {'N','B','D','H','V','Y','S','R','W','K','M','A','C','G','T'}}
-};
-
-void generate_included_motifs_rec(unordered_set<string> &res, const string &motif, string &acc, unsigned int pos, unsigned int l) {
-    if (pos == l) {
-        res.emplace(acc);
-        res.emplace(reverse_complement(acc));
-        return;
-    }
-    for (auto nuc : iupac_to_included_nucs[motif[pos]]) {
-        acc.push_back(nuc);
-        generate_included_motifs_rec(res, motif, acc, pos+1, l);
-        acc.pop_back();
-    }
-}
-
-void generate_included_motifs(unordered_set<string> &res, const string &motif, unsigned int l) {
-    string acc;
-    generate_included_motifs_rec(res, motif, acc, 0, l);
-}
-
 bool motifs_match(const string &motif1, const string &motif2, unsigned int l) {
     unordered_set<string> matching_motif1;
     unordered_set<string> matching_motif2;
@@ -43,20 +7,17 @@ bool motifs_match(const string &motif1, const string &motif2, unsigned int l) {
     if(motif1 == motif2)
         return true;
 
-    generate_included_motifs(matching_motif1, motif1, l);
-    if (find(matching_motif1.begin(), matching_motif1.end(), motif2) != matching_motif1.end()){
-        std::cout << "Motifs " << motif1 << " " << motif2 << " match" << std::endl;
-        return true;
+    else {
+        for (unsigned int pos=0; pos < l; pos++) {
+            unordered_set<char> nuc_intersection;
+            set_intersection(iupac_to_nucs[motif1[pos]].begin(), iupac_to_nucs[motif1[pos]].end(),
+                             iupac_to_nucs[motif2[pos]].begin(), iupac_to_nucs[motif2[pos]].end(),
+                             std::inserter(nuc_intersection,nuc_intersection.begin()));
+            if (nuc_intersection.empty())
+                return false;
+        }
     }
-
-    generate_included_motifs(matching_motif2, motif2, l);
-    if (find(matching_motif2.begin(), matching_motif2.end(), motif1) != matching_motif2.end()){
-        std::cout << "Motifs " << motif1 << " " << motif2 << " match" << std::endl;
-        return true;
-    }
-    // std::cout << "Motifs " << motif1 << " " << motif2 << " don't match" << std::endl;
-    return false;
-
+    return true;
 }
 
 void filter_redundant_motif(vector<pair<const string, pair<int, Node *> > *> &remaining_hash_map_entries, unsigned int l) {

relation_tables.cpp
@@ -1,0 +1,45 @@
+#include "relation_tables.hpp"
+
+const vector<char> nucleotides {'A','C','G','T'};
+
+sparse_hash_map<string, vector<unordered_set<char>>> nucs_to_iupacs {
+    {"ACGT", {{'Y','S','K','R','W','M'}, {'B','D','H','V'}, {'N'}}},
+    {"ACG" , {{'M','R','S'}, {'V'}}},
+    {"ACT" , {{'M','W','Y'}, {'H'}}},
+    {"AGT" , {{'R','W','K'}, {'D'}}},
+    {"CGT" , {{'S','Y','K'}, {'B'}}},
+    {"AC"  , {{'M'}}},
+    {"AG"  , {{'R'}}},
+    {"AT"  , {{'W'}}},
+    {"CG"  , {{'S'}}},
+    {"CT"  , {{'Y'}}},
+    {"GT"  , {{'K'}}}
+};
+
+sparse_hash_map<char, unordered_set<char>> iupacs_dependencies {
+    {'N', {'B','D','H','V'}},
+    {'B', {'Y','S','K'}},
+    {'D', {'R','W','K'}},
+    {'H', {'Y','W','M'}},
+    {'V', {'R','S','M'}},
+    {'R', {'A','G'}},
+    {'Y', {'C','T'}},
+    {'S', {'G','C'}},
+    {'W', {'A','T'}},
+    {'K', {'G','T'}},
+    {'M', {'A','C'}}
+};
+
+sparse_hash_map<char, unordered_set<char>> iupac_to_nucs {
+    {'N', {'A', 'C', 'G', 'T'}},
+    {'B', {'C', 'G', 'T'}},
+    {'D', {'A', 'G', 'T'}},
+    {'H', {'A', 'C', 'T'}},
+    {'V', {'A', 'C', 'G'}},
+    {'Y', {'C', 'T'}},
+    {'S', {'C', 'G'}},
+    {'K', {'G', 'T'}},
+    {'R', {'A', 'G'}},
+    {'W', {'A', 'T'}},
+    {'M', {'A', 'C'}}
+};

relation_tables.hpp
@@ -1,0 +1,35 @@
+#if !defined(__RELATION_TABLES_HPP_INCLUDED__)
+
+    #define  __RELATION_TABLES_HPP_INCLUDED__
+
+    //=====================
+    //Included dependencies
+    //=====================
+
+    #include <unordered_set>
+    using std::unordered_set;
+
+    #include <vector>
+    using std::vector;
+
+    #include <string>
+    using std::string;
+
+    //=====================
+    //  Included libs
+    //=====================
+
+    #include "lib/sparsepp.h"
+    using spp::sparse_hash_map;
+
+    //=====================
+
+    extern const vector<char> nucleotides;
+
+    extern sparse_hash_map<string, vector<unordered_set<char>>> nucs_to_iupacs;
+
+    extern sparse_hash_map<char, unordered_set<char>> iupacs_dependencies;
+
+    extern sparse_hash_map<char, unordered_set<char>> iupac_to_nucs;
+
+#endif

Makefile
@@ -1,7 +1,7 @@
 #CXX = g++
 CXXFLAGS += -std=c++14 -Wall -Ofast
 
-SRCS=main.cpp hash.cpp optionsParser.cpp degenerate.cpp node.cpp mutual_information.cpp fisher_test.cpp graph_simplification.cpp reverse_complement.cpp meme_format.cpp find_redundant_motif.cpp
+SRCS=main.cpp hash.cpp optionsParser.cpp degenerate.cpp node.cpp mutual_information.cpp fisher_test.cpp graph_simplification.cpp reverse_complement.cpp meme_format.cpp find_redundant_motif.cpp relation_tables.cpp
 OBJS=$(subst .cpp,.o,$(SRCS))
 DEPS=$(subst .cpp,.d,$(SRCS))
 

degenerate.cpp
@@ -1,49 +1,5 @@
 #include "degenerate.hpp"
 
-const vector<char> nucleotides {'A','C','G','T'};
-
-sparse_hash_map<string, vector<unordered_set<char>>> nucs_to_iupacs {
-    {"ACGT", {{'Y','S','K','R','W','M'}, {'B','D','H','V'}, {'N'}}},
-    {"ACG" , {{'M','R','S'}, {'V'}}},
-    {"ACT" , {{'M','W','Y'}, {'H'}}},
-    {"AGT" , {{'R','W','K'}, {'D'}}},
-    {"CGT" , {{'S','Y','K'}, {'B'}}},
-    {"AC"  , {{'M'}}},
-    {"AG"  , {{'R'}}},
-    {"AT"  , {{'W'}}},
-    {"CG"  , {{'S'}}},
-    {"CT"  , {{'Y'}}},
-    {"GT"  , {{'K'}}}
-};
-
-sparse_hash_map<char, unordered_set<char>> iupacs_dependencies {
-    {'N', {'B','D','H','V'}},
-    {'B', {'Y','S','K'}},
-    {'D', {'R','W','K'}},
-    {'H', {'Y','W','M'}},
-    {'V', {'R','S','M'}},
-    {'R', {'A','G'}},
-    {'Y', {'C','T'}},
-    {'S', {'G','C'}},
-    {'W', {'A','T'}},
-    {'K', {'G','T'}},
-    {'M', {'A','C'}}
-};
-
-sparse_hash_map<char, unordered_set<char>> iupac_to_nucs {
-    {'N', {'A', 'C', 'G', 'T'}},
-    {'B', {'C', 'G', 'T'}},
-    {'D', {'A', 'G', 'T'}},
-    {'H', {'A', 'C', 'T'}},
-    {'V', {'A', 'C', 'G'}},
-    {'Y', {'C', 'T'}},
-    {'S', {'C', 'G'}},
-    {'K', {'G', 'T'}},
-    {'R', {'A', 'G'}},
-    {'W', {'A', 'T'}},
-    {'M', {'A', 'C'}}
-};
-
 //retourne les nucléotides concaténés dans l'ordre (assuré par une itération sur le vecteur nucleotides)
 const string find_neighbor_motifs(  sparse_hash_map<string, pair<int, Node *>> &motifs,
                                     sparse_hash_map<char, pair<string, Node *>> &neighbor_motifs,

degenerate.hpp
@@ -32,6 +32,8 @@
 
     #include "reverse_complement.hpp"
 
+    #include "relation_tables.hpp"
+
     //=====================
 
     const string find_neighbor_motifs(sparse_hash_map<string, pair<int, Node *>> &, sparse_hash_map<char, pair<string, Node *>> &, const string &, unsigned int);

find_redundant_motif.hpp
@@ -37,6 +37,8 @@
 
     #include "reverse_complement.hpp"
 
+    #include "relation_tables.hpp"
+
     //=====================
 
     void filter_redundant_motif(vector<pair<const string, pair<int, Node *> > *> &, unsigned int);

commit cf496e89389df5ec5c2bb7a4ff63a60ede532a4c

[FIX] use of set_intersection required the sets to be ordered, fixes bug introduced in 9d56cde9997ce2b119f38314c6d1af40403d8d2a

find_redundant_motif.cpp
@@ -9,10 +9,10 @@ bool motifs_match(const string &motif1, const string &motif2, unsigned int l) {
 
     else {
         for (unsigned int pos=0; pos < l; pos++) {
-            unordered_set<char> nuc_intersection;
-            set_intersection(iupac_to_nucs[motif1[pos]].begin(), iupac_to_nucs[motif1[pos]].end(),
-                             iupac_to_nucs[motif2[pos]].begin(), iupac_to_nucs[motif2[pos]].end(),
-                             std::inserter(nuc_intersection,nuc_intersection.begin()));
+            vector<char> nuc_intersection;
+            set_intersection((iupac_to_nucs[motif1[pos]]).begin(), (iupac_to_nucs[motif1[pos]]).end(),
+                             (iupac_to_nucs[motif2[pos]]).begin(), (iupac_to_nucs[motif2[pos]]).end(),
+                             std::back_inserter(nuc_intersection));
             if (nuc_intersection.empty())
                 return false;
         }
@@ -55,7 +55,7 @@ void filter_redundant_motif(vector<pair<const string, pair<int, Node *> > *> &re
                     /* motif      |------|-|
                        base [...]-|------| */
                     if (letter_added_right < side_addition_limit &&
-                        motifs_match(string(base, base.size() - (l-1), l-1), string(motif, 0, l-1), l)) {
+                        motifs_match(string(base, base.size() - (l-1), l-1), string(motif, 0, l-1), l-1)) {
                         base.push_back(motif[l-1]);
                         found_motif = true;
                         letter_added_right++;
@@ -63,7 +63,7 @@ void filter_redundant_motif(vector<pair<const string, pair<int, Node *> > *> &re
                     /* motif |-|-----|
                        base    |-----|-[...] */
                     else if (letter_added_left < side_addition_limit &&
-                             motifs_match(string(base, 0, l-1), string(motif, 1, l-1), l)) {
+                             motifs_match(string(base, 0, l-1), string(motif, 1, l-1), l-1)) {
                         base.insert(0, 1, motif[0]);
                         found_motif = true;
                         letter_added_left++;

meme_format.cpp
@@ -1,23 +1,5 @@
 #include "meme_format.hpp"
 
-sparse_hash_map<char, unordered_set<char>> iupac_or_nuc_to_nucs {
-    {'A', {'A'}},
-    {'C', {'C'}},
-    {'G', {'G'}},
-    {'T', {'T'}},
-    {'R', {'A','G'}},
-    {'Y', {'C','T'}},
-    {'S', {'G','C'}},
-    {'W', {'A','T'}},
-    {'K', {'G','T'}},
-    {'M', {'A','C'}},
-    {'B', {'C','G','T'}},
-    {'D', {'A','G','T'}},
-    {'H', {'A','C','T'}},
-    {'V', {'A','C','G'}},
-    {'N', {'A','C','G','T'}}
-};
-
 void round_to_hundred_thousandth( double &d ) {
     d = (double) floor((d * 100000.0 ) + 0.5) / 100000;
 }
@@ -40,7 +22,7 @@ void generate_exact_successor_motifs_rec(vector<string> &res, const string &moti
         res.emplace_back(acc);
         return;
     }
-    for (auto nuc : iupac_or_nuc_to_nucs[motif[pos]]) {
+    for (auto nuc : iupac_to_nucs[motif[pos]]) {
         acc.push_back(nuc);
         generate_exact_successor_motifs_rec(res, motif, acc, pos+1, l);
         acc.pop_back();

meme_format.hpp
@@ -36,6 +36,8 @@
 
     #include "node.hpp"
 
+    #include "relation_tables.hpp"
+
     //=====================
 
     int create_meme_file(vector<pair<const string, pair<int, Node *> > *> &, sparse_hash_map<string, pair<int, Node *>> &, sparse_hash_map<char, unsigned int> &, unsigned int, string &);

relation_tables.cpp
@@ -30,7 +30,7 @@ sparse_hash_map<char, unordered_set<char>> iupacs_dependencies {
     {'M', {'A','C'}}
 };
 
-sparse_hash_map<char, unordered_set<char>> iupac_to_nucs {
+sparse_hash_map<char, vector<char>> iupac_to_nucs {
     {'N', {'A', 'C', 'G', 'T'}},
     {'B', {'C', 'G', 'T'}},
     {'D', {'A', 'G', 'T'}},
@@ -41,5 +41,9 @@ sparse_hash_map<char, unordered_set<char>> iupac_to_nucs {
     {'K', {'G', 'T'}},
     {'R', {'A', 'G'}},
     {'W', {'A', 'T'}},
-    {'M', {'A', 'C'}}
+    {'M', {'A', 'C'}},
+    {'A', {'A'}},
+    {'C', {'C'}},
+    {'G', {'G'}},
+    {'T', {'T'}}
 };

relation_tables.hpp
@@ -30,6 +30,7 @@
 
     extern sparse_hash_map<char, unordered_set<char>> iupacs_dependencies;
 
-    extern sparse_hash_map<char, unordered_set<char>> iupac_to_nucs;
+    //second member needs to be ordered (ordering used by set_intersection)
+    extern sparse_hash_map<char, vector<char>> iupac_to_nucs;
 
 #endif