569a13152f95a5081dc7cf65d2d061a976ee8f58
Florian Vanhems <florian.vanhems@etudiant.univ-lille1.fr>
2017-06-28 11:56:17 +0200

    [TESTS] Trying to implement tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 set -ev
 if [ "$TRAVIS_BRANCH" = "unstable" ]; then
-	make test
+	TEST_DIR='test'
+
+	DATASETS=''
+	DATASETS+='GATA1 '
+	DATASETS+='KLF1 '
+	DATASETS+='OCT4 '
+	DATASETS+='SOX2 '
+	DATASETS+='STAT3 '
+
+	CTRL_FILE='ctrl.fa'
+	SIG_FILE='peaks.fa'
+
+	if ! [ -f bin/dinamo ]
+		then
+		echo "Error : executable could not be found"
+	fi
+
+	for DATASET in $DATASETS
+	do
+		bin/dinamo -pf $TEST_DIR/$DATASET/$SIG_FILE -nf $TEST_DIR/$DATASET/$CTRL_FILE -l 6 -d 6 -o temp.meme
+		diff temp.meme $TEST_DIR/$DATASET/dinamo_6_6.meme
+		if [ $? -ne 0 ]; then
+        	echo "Failed test; the program did not output the expected file while testing for $DATASET with -l 6 -d 6" >&2
+			return 1;
+    	fi
+		bin/dinamo -pf $TEST_DIR/$DATASET/$SIG_FILE -nf $TEST_DIR/$DATASET/$CTRL_FILE -l 7 -d 3 -o temp.meme
+		diff temp.meme $TEST_DIR/$DATASET/dinamo_7_3.meme
+		if [ $? -ne 0 ]; then
+        	echo "Failed test; the program did not output the expected file while testing for $DATASET with -l 7 -d 3" >&2
+			return 1;
+    	fi
+	done
+	rm temp.meme
+	return 0;
 fi
