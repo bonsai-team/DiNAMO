c41d5c37761832905f6dbc5a2c9e7db7e596af67
noe <laurent.noe@lifl.fr>
2018-02-08 12:12:44 +0100

    [TESTS] Travis and script update

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,8 +6,8 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-CHECK_MARK='\u2714'
-CROSS='\u2716'
+CHECK_MARK="\xe2\x9c\x97\x0a"
+CROSS_MARK="\xe2\x9c\x93\x0a "
 
 TEST_DIR='test'
 
@@ -35,13 +35,14 @@ do
     do
         for (( d=0; d<=$l; d++ ))
         do
-            echo -e "\t Testing ChIP-Seq mode with parameters -l ${l}, -d ${d}..."
-            bin/dinamo -pf ${TEST_DIR}/${DATASET}/${SIG_FILE} -nf ${TEST_DIR}/${DATASET}/${CTRL_FILE} -l ${l} -d ${d} -o  ${TEST_DIR}/${DATASET}/chipseq_l${l}_d${d}_test.meme > /dev/null
+            echo -en "\t Testing ChIP-Seq mode with parameters -l ${l}, -d ${d}..."
+            bin/dinamo -pf ${TEST_DIR}/${DATASET}/${SIG_FILE} -nf ${TEST_DIR}/${DATASET}/${CTRL_FILE} -l ${l} -d ${d} -o  ${TEST_DIR}/${DATASET}/chipseq_l${l}_d${d}_test.meme 2> /dev/null 1> /dev/null
             diff ${TEST_DIR}/${DATASET}/chipseq_l${l}_d${d}_test.meme  ${TEST_DIR}/${DATASET}/chipseq_l${l}_d${d}.meme
             if [ $? -ne 0 ]; then
-                echo -e "${RED}${CROSS}${NC}\t Test failed; the program did not output the expected file." >&2
+                echo -e "${RED}${CROSS_MARK}${NC}\t Test failed; the program did not output the expected file." >&2
                 exit 1
             fi
+	    printf "\t${GREEN}${CHECK_MARK}${NC}"
             rm -f ${TEST_DIR}/${DATASET}/chipseq_l${l}_d${d}_test.meme
         done
     done
@@ -52,13 +53,14 @@ do
         do
             for p in 1 2 3 4
             do
-                echo -e "\t Testing fixed position mode with parameters -l ${l}, -d ${d}, -p ${p}..."
+                echo -en "\t Testing fixed position mode with parameters -l ${l}, -d ${d}, -p ${p}..."
                 bin/dinamo -pf ${TEST_DIR}/${DATASET}/${SIG_FILE}  -nf ${TEST_DIR}/${DATASET}/${SIG_FILE}  -l ${l} -d ${d} -p ${p} --no-log >  ${TEST_DIR}/${DATASET}/position_l${l}_d${d}_p${p}_test.res
                 diff ${TEST_DIR}/${DATASET}/position_l${l}_d${d}_p${p}_test.res ${TEST_DIR}/${DATASET}/position_l${l}_d${d}_p${p}.res
                 if [ $? -ne 0 ]; then
-                    echo -e "${RED}${CROSS}${NC}\t Test failed; the program did not output the expected file." >&2
+                    echo -e "${RED}${CROSS_MARK}${NC}\t Test failed; the program did not output the expected file." >&2
                     exit 1
                 fi
+		echo -e "\t${GREEN}${CHECK_MARK}${NC}"
                 rm -f ${TEST_DIR}/${DATASET}/position_l${l}_d${d}_p${p}_test.res
             done
         done
