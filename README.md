c105b0889f7e08090a91d4626ff9b06d4ffdb60d
chadisaad <chadi.saad@hotmail.com>
2018-04-06 16:38:55 +0200

    README

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
+
 # DiNAMO: An exact and efficient method for IUPAC motif discovery in DNA sequences
 
-[![Build Status](https://travis-ci.org/bonsai-team/DiNAMO.svg?branch=master)](https://travis-ci.org/bonsai-team/DiNAMO)  [![Build status](https://ci.appveyor.com/api/projects/status/a9powubl5mqeigvt?svg=true)](https://ci.appveyor.com/project/chadisaad/dinamo)
+[![Build Status](https://travis-ci.org/bonsai-team/DiNAMO.svg?branch=master)](https://travis-ci.org/bonsai-team/DiNAMO) [![Build status](https://ci.appveyor.com/api/projects/status/a9powubl5mqeigvt/branch/master?svg=true)](https://ci.appveyor.com/project/chadisaad/dinamo/branch/master)
 
 The DiNAMO software implements an exhaustive algorithm to detect over-represented IUPAC motifs in a set of DNA sequences. It has two modes: *scanning* mode (default), where all windows are parsed, or *fixed-position* mode (see optional parameter *-p* ), where only motifs occurring at a specific position in the sequences are taken into account.  
 
 DiNAMO can be used in a variety of applications, such as ChIP-seq peak analysis for transcription factor binding sites identification, or finding motifs that induce systematic sequencing errors.
 
-## Dependencies
+## Installation
+
+### Binaries
+Binaries for Windows, OS X and Linux are available [here](https://github.com/bonsai-team/DiNAMO/releases).
+
+### Build from source code
+#### Dependencies
 
     Boost library (if not in the standard includes, please use -I /path/to/boost in the Makefile)
 
 
-## Compilation
+#### Compilation
     make
 
 The executable file is located in the bin/ directory
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -21,7 +21,9 @@ DATASETS+='STAT3 '
 CTRL_FILE='ctrl.fa'
 SIG_FILE='peaks.fa'
 
-if ! [ -f bin/dinamo ]
+DINAMO_PATH="bin/${BINARY_NAME}"
+
+if ! [ -f $DINAMO_PATH ]
 then
     echo -e "${RED}Error : executable could not be found${NC}"
     exit 1
@@ -36,7 +38,7 @@ do
         for (( d=0; d<=$l; d++ ))
         do
             echo -en "\t Testing ChIP-Seq mode with parameters -l ${l}, -d ${d}..."
-            bin/dinamo -pf ${TEST_DIR}/${DATASET}/${SIG_FILE} -nf ${TEST_DIR}/${DATASET}/${CTRL_FILE} -l ${l} -d ${d} -o  ${TEST_DIR}/${DATASET}/chipseq_l${l}_d${d}_test.meme 2> /dev/null 1> /dev/null
+            $DINAMO_PATH -pf ${TEST_DIR}/${DATASET}/${SIG_FILE} -nf ${TEST_DIR}/${DATASET}/${CTRL_FILE} -l ${l} -d ${d} -o  ${TEST_DIR}/${DATASET}/chipseq_l${l}_d${d}_test.meme 2> /dev/null 1> /dev/null
             diff -w ${TEST_DIR}/${DATASET}/chipseq_l${l}_d${d}_test.meme  ${TEST_DIR}/${DATASET}/chipseq_l${l}_d${d}.meme
             if [ $? -ne 0 ]; then
                 echo -en "${RED}${CROSS_MARK}${NC}\t Test failed; the program did not output the expected file." >&2
@@ -54,7 +56,7 @@ do
             for p in 1 2 3 4
             do
                 echo -en "\t Testing fixed position mode with parameters -l ${l}, -d ${d}, -p ${p}..."
-                bin/dinamo -pf ${TEST_DIR}/${DATASET}/${SIG_FILE}  -nf ${TEST_DIR}/${DATASET}/${CTRL_FILE}  -l ${l} -d ${d} -p ${p} --no-log >  ${TEST_DIR}/${DATASET}/position_l${l}_d${d}_p${p}_test.res
+                $DINAMO_PATH -pf ${TEST_DIR}/${DATASET}/${SIG_FILE}  -nf ${TEST_DIR}/${DATASET}/${CTRL_FILE}  -l ${l} -d ${d} -p ${p} --no-log >  ${TEST_DIR}/${DATASET}/position_l${l}_d${d}_p${p}_test.res
                 diff -w ${TEST_DIR}/${DATASET}/position_l${l}_d${d}_p${p}_test.res ${TEST_DIR}/${DATASET}/position_l${l}_d${d}_p${p}.res
                 if [ $? -ne 0 ]; then
                     echo -en "${RED}${CROSS_MARK}${NC}\t Test failed; the program did not output the expected file." >&2
