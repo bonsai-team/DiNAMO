3a6ec5cffbce4b2cb291eba3e9c4c820e107931a
chadisaad <chadi.saad@hotmail.com>
2018-04-06 16:53:25 +0200

    README

--- delete_tags.sh
+++
@@ -1,8 +0,0 @@
-#Delete local tags.
-git tag -d $(git tag -l)
-#Fetch remote tags.
-git fetch
-#Delete remote tags.
-git push origin --delete $(git tag -l) # Pushing once should be faster than multiple times
-#Delete local tags.
-git tag -d $(git tag -l)
